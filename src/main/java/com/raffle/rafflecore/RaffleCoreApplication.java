fc3e4c3e66c3af7d18495cf49d7e1e7c2fe7ef5c
Cristian Piña <cristian-torres1@outlook.com>
2023-04-01 22:49:17 -0500

    first project

---
+++ src/main/java/com/raffle/rafflecore/RaffleCoreApplication.java
@@ -0,0 +1,13 @@
+package com.raffle.rafflecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffleCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raffle/rafflecore/RaffleCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raffle.rafflecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffleCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
